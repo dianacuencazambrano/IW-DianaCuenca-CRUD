41de12214229a78acba957bcfca701a4837b9b49
DianaCuenca <diana.cuenca.zambrano@udla.edu.ec>
2022-10-08 20:34:23 -0500

    exceptions control in users methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,23 +15,33 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::get();
-        return view('users.index', compact('users'));
+        try {
+            $users = User::get();
+            return view('users.index', [
+                'users' => $users
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th], 400);
+        }
     }
 
     public function show($user)
     {
-        $user = User::find($user);
-        if($user->status == 1){
-            $action = "Disable";
-        }else{
-            $action = "Enable";
+        try {
+            $user = User::find($user);
+            if ($user->status == 1) {
+                $action = "Disable";
+            } else {
+                $action = "Enable";
+            }
+
+            return view('users.show', [
+                'user'  => $user,
+                'action' => $action,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th], 400);
         }
-            
-        return view('users.show', [
-            'user'  => $user,
-            'action' => $action,
-        ]);
     }
 
     public function create()
@@ -41,40 +51,61 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        User::create($request->all());
-        //return $this->index();
-        return redirect()->route('users.index')->with(['success' => '¡¡Usuario Creado Exitosamente!!']);
+        try {
+            $user = new User();
+            $user->name = $request->input('name');
+            $user->lastname = $request->input('lastname');
+            $user->birthday = $request->input('birthday');
+            $user->identification = $request->input('identification');
+            $user->phoneNumber = $request->input('phoneNumber');
+            $user->homeAddress = $request->input('homeAddress');
+            $user->email = $request->input('email');
+            $user->password  = bcrypt($request->input('password'));
+            $user->save();
+            return redirect()->route('users.index')->with(['success' => '¡¡Usuario Creado Exitosamente!!']);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th], 400);
+        }
     }
 
     public function edit($user)
     {
-        $user = User::find($user);
-        return view('users.edit', compact('user'));
+        try {
+            $user = User::find($user);
+            return view('users.edit', compact('user'));
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th], 400);
+        }
     }
 
     public function update(Request $request)
     {
-        $user = User::find($request->id);
-        $user->name = $request->name;
-        $user->lastname = $request->lastname;
-        $user->email = $request->email;
-        $user->save();
-        
-        return $this->index();
+        try {
+            $user = User::find($request->id);
+            $user->name = $request->name;
+            $user->lastname = $request->lastname;
+            $user->email = $request->email;
+            $user->save();
+            return $this->index();
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th], 400);
+        }
     }
 
     public function changeStatus($user)
     {
-        $user = User::find($user);
-        if($user->status == 1)
-        {
-            $user->status = 0;
-            $user->save();
-        }else
-        {
-            $user->status = 1;
-            $user->save();
+        try {
+            $user = User::find($user);
+            if ($user->status == 1) {
+                $user->status = 0;
+                $user->save();
+            } else {
+                $user->status = 1;
+                $user->save();
+            }
+            return $this->index();
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th], 400);
         }
-        return $this->index();
     }
 }
