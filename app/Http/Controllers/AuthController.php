c62a1c207def4aeb54b491158b1612d81bfcd9ca
DianaCuenca <diana.cuenca.zambrano@udla.edu.ec>
2022-10-09 22:46:04 -0500

    auth controller with methods to login and logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $remember = $request->filled('remember');
+        if (Auth::attempt($credentials, $remember)) {
+            request()->session()->regenerate();
+            return redirect()->intended('/');
+        } else {
+            throw ValidationException::withMessages([
+                'email' => __('auth.failed')
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->to('/welcome');
+    }
+}
