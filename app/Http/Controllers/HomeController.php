2fb269f6e96a897694e2f6cd210161a0bcd4227d
DianaCuenca <diana.cuenca.zambrano@udla.edu.ec>
2022-12-14 00:45:31 -0500

    code of core advance qual per stud

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Classroom;
+use App\Models\Skill_Qual_Stud;
+use App\Models\Skill_Rein_Stud;
 use App\Models\Student;
 use App\Models\Qualification;
 use Illuminate\Http\Request;
@@ -22,12 +24,34 @@ class HomeController extends Controller
     }
 
     public function home(){
-        $students = Student::all();
+        $id_user = Auth::user()->id_user;
+        $student = Student::where('id_user',$id_user)->get();
+        $quals = Skill_Qual_Stud::where('id_stud', $student[0]->id_stud)->get();
+        //$rein = (Skill_Rein_Stud::where('id_stud', $student[0]->id_stud)->get()) ? Skill_Rein_Stud::where('id_stud',$student[0]->id_stud)->get() : '';
+        $aux = [];
 
-        if(Auth::user()->id_role == 2)
+        if(Auth::user()->id_role == 3){
+            foreach($quals as $key => $value){
+                if($value->id_qual == 2) {
+                    array_push($aux, $value);
+                }
+            }
 
+            if(!empty($aux)){
+                if(!empty($rein)){
+                    foreach ($aux as $key => $value) {
+                        dd($value);
+                    }
+                }
+
+                
+            }
+
+            
+        }
         return view('home',[
-            'students' => $students
+            'student' => $student,
+            'quals' => $quals
         ]);
     }
 
