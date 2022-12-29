4df5a0a4c42f13ad3c9c1c9da8cf47f81d225791
DianaCuenca <diana.cuenca.zambrano@udla.edu.ec>
2022-12-28 21:28:36 -0500

    reinforcements showing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,35 +23,60 @@ class HomeController extends Controller
         }
     }
 
+    // public function home(){
+    //     $id_user = Auth::user()->id_user;
+    //     $student = [];
+    //     $quals = [];
+    //     $rein = [];	
+    //     $aux = [];
+
+    //     if(Auth::user()->id_role == 3){
+    //         $student = Student::where('id_user',$id_user)->get();
+    //         $quals = Skill_Qual_Stud::where('id_stud', $student[0]->id_stud)->get();
+    //         $rein = Skill_Rein_Stud::where('id_stud', $student[0]->id_stud)->get();
+    //         //dd($rein);
+            
+    //         foreach($quals as $key => $value){
+    //             if($value->id_qual == 2) {
+    //                 array_push($aux, $value);
+    //             }
+    //         }
+
+    //         if(!empty($aux)){
+    //             if(empty($rein)){
+    //                 foreach ($aux as $key => $value) {
+    //                     dd($value);
+    //                 }
+    //             }
+    //         }            
+    //     }
+    //     //dd($aux);
+        
+    //     return view('home',[
+    //         'student' => $student,
+    //         //'quals' => $aux,
+    //         'reinforcements' => $rein
+    //     ]);
+    // }
+
     public function home(){
         $id_user = Auth::user()->id_user;
-        $student = Student::where('id_user',$id_user)->get();
-        $quals = Skill_Qual_Stud::where('id_stud', $student[0]->id_stud)->get();
-        //$rein = (Skill_Rein_Stud::where('id_stud', $student[0]->id_stud)->get()) ? Skill_Rein_Stud::where('id_stud',$student[0]->id_stud)->get() : '';
+        $student = [];
+        $quals = [];
+        $rein = [];	
         $aux = [];
 
         if(Auth::user()->id_role == 3){
-            foreach($quals as $key => $value){
-                if($value->id_qual == 2) {
-                    array_push($aux, $value);
-                }
-            }
-
-            if(!empty($aux)){
-                if(!empty($rein)){
-                    foreach ($aux as $key => $value) {
-                        dd($value);
-                    }
-                }
-
+            $rein = Skill_Rein_Stud::where('id_stud', $student[0]->id_stud)->get();       
+            foreach($rein as $r){
                 
-            }
-
-            
+            } 
         }
+        
         return view('home',[
             'student' => $student,
-            'quals' => $quals
+            //'quals' => $aux,
+            'reinforcements' => $rein
         ]);
     }
 
